b3a74580bd76c4abd61816c891f05cd9d4a52d3a
xubinggui <xubinggui@zepplabs.com>
2016-08-28 15:32:06 +0800

    Add Decorate

---
+++ DesignPatterns/decorate/src/main/java/com/example/Mocha.java
@@ -0,0 +1,39 @@
+package com.example;
+
+//  Created by xubinggui on 8/28/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class Mocha extends CondimentDecorator {
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override public double cost() {
+        return 0.20 + beverage.cost();
+    }
+}
---
+++ DesignPatterns/decorate/src/main/java/com/example/StarbuzzCoffee.java
@@ -0,0 +1,34 @@
+package com.example;
+
+//  Created by xubinggui on 8/28/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class StarbuzzCoffee {
+    public static void main(String[] args) {
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        Beverage beverage1 = new HouseBlend();
+        beverage1 = new Mocha(beverage1);
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+    }
+}
