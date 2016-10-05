facfbd3f390aebcc42c0d5204e644bb0f70cb03c
xubinggui <xubinggui@zepplabs.com>
2016-10-05 14:55:05 +0800

    Add Facade

---
+++ DesignPatterns/facade/src/main/java/com/example/DwarvenCartOperator.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * 
+ * DwarvenCartOperator is one of the goldmine subsystems.
+ *
+ */
+public class DwarvenCartOperator extends DwarvenMineWorker {
+
+  @Override
+  public void work() {
+    System.out.println(name() + " moves gold chunks out of the mine.");
+  }
+
+  @Override
+  public String name() {
+    return "Dwarf cart operator";
+  }
+}
---
+++ DesignPatterns/facade/src/main/java/com/example/DwarvenGoldDigger.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * 
+ * DwarvenGoldDigger is one of the goldmine subsystems.
+ *
+ */
+public class DwarvenGoldDigger extends DwarvenMineWorker {
+
+  @Override
+  public void work() {
+    System.out.println(name() + " digs for gold.");
+  }
+
+  @Override
+  public String name() {
+    return "Dwarf gold digger";
+  }
+}
